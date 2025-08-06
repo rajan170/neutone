e849b4749570c19ce69c7511dff7fb572139ec57
Rajan <rajan.bangar83@gmail.com>
2025-08-05 23:17:30 -0400

    main entrypoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
+import modal
+import os
+from modal_config import app, image, neutone_secrets, model_volume
+
+@app.cls(
+    image=image,
+    gpu="L40S",
+    volumes={"/models": model_volume}
+)
+class MusicGenServer:
+    def load_model(self):
+        pass
+
+    
+
+@app.function(image=image, secrets=[neutone_secrets])
+def test_func():
+    print("Hello, World!")
+    print(os.environ["test"])
+
+@app.local_entrypoint()
 def main():
-    print("Hello from backend!")
-
-
-if __name__ == "__main__":
-    main()
+    test_func.remote()
