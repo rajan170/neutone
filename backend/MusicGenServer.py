42287d9d5f6d0214ec51088f7c9811cd588ece27
Rajan <rajan.bangar83@gmail.com>
2025-08-06 18:51:00 -0400

    feat: add music generation with base64 encoding

--- backend/MusicGenServer.py
+++ backend/MusicGenServer.py
@@ -1,9 +1,12 @@
+import base64
+import uuid
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from diffusers import AutoPipelineForText2Image
 from acestep.pipeline_ace_step import ACEStepPipeline
 import torch
-
-from modal_config import app, image
+import os
+import modal
+from modal_config import app, image, model_volume, hf_volume, neutone_secrets
 
 @app.cls(
     image=image,
@@ -38,7 +41,32 @@ class MusicGenServer:
         )
 
         # Stable Diffusion Model (for generating thumbnails)
-        pipe = AutoPipelineForText2Image.from_pretrained(
-            "stabilityai/sdxl-turbo", torch_dtype="torch.float16", variant="fp16", cache_dir="/root/cache/huggingface"
+        self.image_pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo",
+            torch_dtype="torch.float16", 
+            variant="fp16", 
+            cache_dir="/root/cache/huggingface"
         )
-        pipe.to("cuda")
+        self.image_pipe.to("cuda")
+    
+    @modal.fastapi_endpoint(method="POST")
+    def generate(self):
+        output_dir = "/tmp/outputs"
+        os.mkdir(output_dir, exist_ok = True)
+        output_path = os.path.join(output_dir, f"{uuid.uuid4()}.wav")
+
+        self.music_model(
+            prompt = "A song about a cat, billie ellish",
+            lyrics = "a cat's life",
+            audio_duration = 100,
+            infer_step = 60,
+            guidance_scale = 15,
+            save_path = output_path,
+        )
+
+        with open(output_path, "rb") as f:
+            audio_bytes = f.read()
+
+            audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
+
+        os.remove(output_path)
