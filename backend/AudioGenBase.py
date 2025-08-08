9c72ecdf1bd17ca3bd3ecd9d710be4b22d4b78ba
Rajan <rajan.bangar83@gmail.com>
2025-08-08 02:27:14 -0400

    feat: add AudioGenBase models for music generation endpoints

---
+++ backend/AudioGenBase.py
@@ -0,0 +1,28 @@
+from sre_parse import CATEGORIES
+from typing import List
+from pydantic import BaseModel
+
+class AudioGenerationBase(BaseModel):
+    audio_duration: float = 180
+    seed: int = -1
+    guidance_scale: float = 15.0
+    infer_step: int = 60
+
+class GenerateFromDescriptionRequest(AudioGenerationBase):
+    full_described_song: str
+
+class GenerateWithCustomLyricsRequest(AudioGenerationBase):
+    prompt: str 
+    lyrics: str
+
+class GenerateDescribedLyricsRequest(AudioGenerationBase):
+    prompt: str
+    described_lyrics: str
+
+class GenerateMusicResponseS3(BaseModel):
+    s3_key: str 
+    cover_img_s3_key: str
+    categories: List[str]
+
+class GenerateMusicResposne(BaseModel):
+    audio_data: str
