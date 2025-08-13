21dd28e1c526bea003b72ec54326cff2f9b7533a
Rajan <rajan.bangar83@gmail.com>
2025-08-13 02:15:15 -0400

    feat:added auth for modal

--- backend/main.py
+++ backend/main.py
@@ -13,43 +13,29 @@ from dotenv import load_dotenv
 
 @app.local_entrypoint()
 def main():
-    # load_dotenv()
+    load_dotenv()
 
-    # # Header name defaults to Modal's proxy auth header; can be overridden via env
-    # modal_auth_header_name = os.getenv("MODAL_AUTH_HEADER", "Modal-Secret")
-    # modal_secret_value = os.getenv("MODAL_SECRET")
+    modal_key = os.getenv("Modal-Key")
+    modal_secret = os.getenv("Modal-Secret")
 
-    # if not modal_secret_value:
-    #     raise RuntimeError(
-    #         "Missing MODAL_SECRET environment variable. Set it in your environment or .env file."
-    #     )
-
-
-    
-    headers = {"Modal-Key": "*****************",
-                "Modal-Secret": "*****************"}
+    headers = {"Modal-Key": modal_key,
+                "Modal-Secret": modal_secret}
 
 
     server = MusicGenServer()
     endpoint_url = server.generate_from_described_lyrics.get_web_url()
 
-    
-
+    # Generate music from described lyrics
     request = GenerateDescribedLyricsRequest(
         prompt="trap, hiphop, gangsta rap, 140BPM",
         described_lyrics="lyrics about a boy from the hood",
         guidance_scale=15,
     )
 
-    payload = request.model_dump()
+    payload = request.model_dump()  # Convert the request to a dictionary 
 
     response = requests.post(endpoint_url, json=payload, headers=headers)
     response.raise_for_status()
     result = GenerateMusicResponseS3(**response.json())
 
     print(f"Success: {result.s3_key} {result.cover_img_s3_key} {result.categories}")
-
-    # audio_bytes = base64.b64decode(result.audio_data)
-    # output_filename = "generated_music.wav"
-    # with open(output_filename, "wb") as f:
-    #     f.write(audio_bytes)
