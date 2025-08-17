0f0665eebecddfd6d7e1c2ed337315478ffc23fb
Rajan <rajan.bangar83@gmail.com>
2025-08-17 18:11:54 -0400

    feat: added sidebar and breadcrumbs for navigation

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,6 +1,24 @@
 import "~/styles/globals.css";
 
 import { Geist } from "next/font/google";
+import { Providers } from "../components/providers";
+import { Toaster } from "sonner";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/ui/sidebar";
+import { AppSidebar } from "../components/sidebar/app-sidebar";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage, BreadcrumbSeparator
+} from "~/components/ui/breadcrumb";
+import { Separator } from "~/components/ui/separator";
+import BreadcrumbPageClient from "../components/sidebar/breadcrumb-page-client";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -13,7 +31,31 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        {children}
+        <Providers>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset className="flex h-screen flex-col">
+              <header className="bg-background sticky-top z-10 border-b px-4 py-2">
+                <div className="flex shrink-0 grow items-center gap-2">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  <Breadcrumb>
+                    <BreadcrumbList>
+                      <BreadcrumbItem>
+                        <BreadcrumbPageClient />
+                      </BreadcrumbItem>
+                    </BreadcrumbList>
+                  </Breadcrumb>
+                </div>
+              </header>
+              <main className="flex-1 overflow-y-auto">{children}</main>
+            </SidebarInset>
+          </SidebarProvider>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
