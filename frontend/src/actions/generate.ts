d79a41be3445054b0b508ff63c285126a6950a44
Rajan <rajan.bangar83@gmail.com>
2025-08-20 21:48:35 -0400

    feat: hooked up the create button to the backend

--- frontend/src/actions/generate.ts
+++ frontend/src/actions/generate.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { inngest } from "~/inngest/client";
@@ -7,32 +8,57 @@ import { auth } from "~/lib/auth"
 import { db } from "~/server/db";
 
 
-export async function queueSong() {
-    try {
-        const session = await auth.api.getSession({
-            headers: await headers(),
-        });
-
-        if (!session) redirect("/auth/sign-in");
-
-        const song = await db.song.create({
-            data: {
-                userId: session.user.id,
-                title: "Test Song-1",
-                fullDescribedSong: "A song about a cat's life in the city",
-            },
-        });
-
-        await inngest.send({
-            name: "generate-song-event",
-            data: {
-                songId: song.id,
-                userId: song.userId,
-            }
-        });
-
-        console.log("Event sent successfully");
-    } catch (error) {
-        console.error(error);
-    }
+export interface GenerateRequest {
+    prompt?: string;
+    lyrics?: string;
+    fullDescribedSong?: string;
+    instrumental?: boolean;
+    describedLyrics?: string;
 }
+
+export async function genarateSong(generateRequest: GenerateRequest) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) redirect("/auth/sign-in");
+
+    await queueSong(generateRequest, 7.5, session.user.id)
+    await queueSong(generateRequest, 15, session.user.id)
+
+    revalidatePath("/create");
+
+}
+
+
+export async function queueSong(generateRequest: GenerateRequest, guidanceScale: number, userId: string) {
+
+    let title = "Untitled"
+    if (generateRequest.describedLyrics) title = generateRequest.describedLyrics
+    if (generateRequest.fullDescribedSong) title = generateRequest.fullDescribedSong
+
+    title = title.charAt(0).toUpperCase() + title.slice(1)
+
+    const song = await db.song.create({
+        data: {
+            userId: userId,
+            title: title,
+            prompt: generateRequest.prompt,
+            lyrics: generateRequest.lyrics,
+            fullDescribedSong: generateRequest.fullDescribedSong,
+            instrumental: generateRequest.instrumental,
+            describedLyrics: generateRequest.describedLyrics,
+            guidanceScale: guidanceScale,
+            audioDuration: 180,
+        }
+    })
+
+
+    await inngest.send({
+        name: "generate-song-event",
+        data: {
+            songId: song.id,
+            userId: song.userId,
+        }
+    });
+} 
